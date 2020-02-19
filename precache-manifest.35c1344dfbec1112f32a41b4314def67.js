b5c3520fa2429d804c8452bed7c6677201b1133e
unknown <dimagumball@gmail.com>
2020-02-20 00:38:45 +0200

    Updates

---
+++ precache-manifest.35c1344dfbec1112f32a41b4314def67.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b94f3fac43b1ad36dfcc2bab8b844e9",
+    "url": "/WarcraftOneLove/index.html"
+  },
+  {
+    "revision": "435d167d737771b996ad",
+    "url": "/WarcraftOneLove/static/css/main.0040fbea.chunk.css"
+  },
+  {
+    "revision": "b8e33492a12af7f30eee",
+    "url": "/WarcraftOneLove/static/js/2.e1225c87.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/WarcraftOneLove/static/js/2.e1225c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435d167d737771b996ad",
+    "url": "/WarcraftOneLove/static/js/main.061cbed3.chunk.js"
+  },
+  {
+    "revision": "5dab5a004055604f3163",
+    "url": "/WarcraftOneLove/static/js/runtime-main.117c85a7.js"
+  },
+  {
+    "revision": "1dd8d81c8ec73dac8887c1243a771e56",
+    "url": "/WarcraftOneLove/static/media/LifeCraft_Font_0.1dd8d81c.ttf"
+  },
+  {
+    "revision": "cc66e086ec3046582a52c21af263ebbd",
+    "url": "/WarcraftOneLove/static/media/elf-borders.cc66e086.png"
+  },
+  {
+    "revision": "27154173cd26ee07f82456d300a37111",
+    "url": "/WarcraftOneLove/static/media/fons-ork.27154173.webp"
+  },
+  {
+    "revision": "0eb8aca53ab593c81cd1f9e6d7d65453",
+    "url": "/WarcraftOneLove/static/media/human-borders.0eb8aca5.png"
+  },
+  {
+    "revision": "70c0b19d6ae7d51bf05ae7bf6c16280c",
+    "url": "/WarcraftOneLove/static/media/orb-borders.70c0b19d.png"
+  }
+]);
